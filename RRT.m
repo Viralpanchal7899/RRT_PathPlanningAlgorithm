e7e3603cab5461adcbc9d47a6a4296e970dd82be
Viral Panchal <viralpanchal1999@gmail.com>
2022-04-17 11:35:38 -0400

    prints proper angles

--- RRT.m
+++ RRT.m
@@ -7,27 +7,33 @@ py1 = start_state(1,2);
 tree_points = zeros();
 tree_points(1,1) = px1(1,1);
 tree_points(1,2) = py1(1,1);
-px_new(1,1) = px1(1,1);
-py_new(1,2) = py1(1,1);
+parent_new(1,1) = zeros();
+parent_new(1,2) = zeros();
 
-px2 = round(rand(1) * Px_max);
-py2 = round(rand(1) * Py_max);
+rand_mat = zeros();
 i = 1;
+collision_stat = zeros;
+j=1;
 while tree_points(i,1)<90
+rand_mat(i,1) = round(rand(1) * Px_max); 
+rand_mat(i,2) = round(rand(1) * Py_max);
+px2 = rand_mat(i,1);
+py2 = rand_mat(i,2);
 nn_index = nearest_neighbour(tree_points,px2,py2);
 px1 = tree_points(nn_index,1);
 py1 = tree_points(nn_index,2);
-
 is_colliding = collision_check_segment(px1,py1,px2,py2,obstacles);
+collision_stat(j,1) = is_colliding;
 if  is_colliding == 0
-    slope_m = (py2 - py1)/(px2 - px1)
+    parent_new(i,1) = px1;
+    parent_new(i,2) = py1;
+    slope_m = (py2 - py1)/(px2 - px1);
     theta = atand(slope_m);
     i = i + 1;
-    tree_points(i,1) = e*cosd(theta);
-    tree_points(i,2) = e*sind(theta);
+    tree_points(i,1) = px1 + e*cosd(theta);
+    tree_points(i,2) = py1 + e*sind(theta);
 end
-px2 = round(rand(1) * Px_max);
-py2 = round(rand(1) * Py_max);
+j=j+1;
 end
 hold on
-plot(tree_points(:,1),tree_points(:,2));
+plot(parent_new(:,1),parent_new(:,2));
